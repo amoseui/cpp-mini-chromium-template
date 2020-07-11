a74ae3b3d824df414c7c9a869e7bc6f293b8277b
Amos Lim <amoseui@gmail.com>
2020-07-11 13:17:37 +0900

    Test delete array

--- test/hello_test.cc
+++ test/hello_test.cc
@@ -2,15 +2,31 @@
 #include "hello_static.h"
 
 TEST(SampleTest, TestGetStaticText) {
-  ASSERT_EQ("world", GetStaticText());
+    ASSERT_EQ("world", GetStaticText());
 }
 
 TEST(SampleTest, TestGetStaticNumber) {
-  ASSERT_EQ(1, GetStaticNumber());
+    ASSERT_EQ(1, GetStaticNumber());
+}
+
+TEST(SampleTest, TestPointer) {
+    uint* test = new uint[8];
+    test[0] = 1000;
+    test[1] = 2000;
+    delete[] test;
+    test = 0;
+
+    if (test != 0) {
+        delete[] test;
+        test = 0;
+    }
+    test = new uint[8];
+
+    ASSERT_EQ(0, 0);
 }
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
 
